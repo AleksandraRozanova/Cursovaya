b1cb330b96112b9151462c4390de286388df6b93
AleksandraRozanova <83225801+AleksandraRozanova@users.noreply.github.com>
2022-01-26 22:18:13 +0300

    соединение с бд

--- connectdb.php
+++ connectdb.php
@@ -1,6 +1,3 @@
 <?php
 $connect = mysqli_connect("std-mysql.ist.mospolytech.ru", "std_1602_curs", "Rs020926", "std_1602_curs");
-// $sth = $connect->prepare("SELECT * FROM `0` ORDER BY `ObjectName`");
-// $sth->execute();
-// $list = $sth->fetchAll(PDO::FETCH_ASSOC);
 ?>
